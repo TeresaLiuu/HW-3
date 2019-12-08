214ff6fe7144a022d59033a031f08aa1d526a47f
Teresa Liu <kuas033@gmail.com>
2019-12-08 00:00:29 -0800

    generate checkbox

---
+++ script.js
@@ -0,0 +1,37 @@
+function genpassword() {
+  var lowerEl = document.getElementById("lowercase").checked;
+  var upperEl = document.getElementById("uppercase").checked;
+  var numbersEl = document.getElementById("numbers").checked;
+  var symbolsEl = document.getElementById("symbols").checked;
+
+
+  var number = document.getElementById("length").value;
+  var inputbox = document.getElementById("InputPassword");
+
+  var chars = "";
+  if (lowerEl == true){
+    chars+="acdefghijklmnopqrstuvwxyz";
+  }
+  if (upperEl == true){
+    chars+="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (numbersEl == true){
+    chars+="0123456789";
+  }
+  if (symbolsEl == true){
+    chars+="#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  }
+
+  
+  var result = "";
+  for (var i = 0; i < number; i++) {
+    var random = Math.floor(Math.random() * chars.length);
+    result += chars.charAt(random);
+  }
+  inputbox.value = result;
+}
+
+
+
+
+
